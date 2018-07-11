dde5ff91db23fa6759d64fbca0032736d0137563
elena.mikheiko <elena.mikheiko@gmail.com>
2018-07-11 16:29:19 +0300

    First commit. The project structure has been created

---
+++ Source/RentABike.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentABike.Website.Startup))]
+namespace RentABike.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
