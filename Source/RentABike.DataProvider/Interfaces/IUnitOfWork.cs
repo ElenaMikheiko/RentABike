b4beb554b3a69400c3f587d39baf85e63ecd2d46
Elena <elena.mikheiko@gmail.com>
2018-08-01 02:05:04 +0300

    Added Unit of work in DataProvider project

---
+++ Source/RentABike.DataProvider/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RentABike.DataProvider.Repositories;
+using RentABike.Models;
+
+namespace RentABike.DataProvider.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        GenericRepository<Bike> BikeRepository { get; }
+
+        GenericRepository<RentPoint> RentPointRepository { get; }
+
+        GenericRepository<BikeType> BikeTypeRepository { get; }
+
+        void Save();
+    }
+}
---
+++ Source/RentABike.DataProvider/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using RentABike.DataProvider.Interfaces;
+using RentABike.DataProvider.Repositories;
+using RentABike.Models;
+
+namespace RentABike.DataProvider
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly RentABikeDbContext _dbContext;
+
+        private bool _disposed;
+
+        public UnitOfWork()
+        {
+            _dbContext = new RentABikeDbContext();
+        }
+
+        public GenericRepository<Bike> BikeRepository => new GenericRepository<Bike>(_dbContext);
+
+        public GenericRepository<RentPoint> RentPointRepository => new GenericRepository<RentPoint>(_dbContext);
+
+        public GenericRepository<BikeType> BikeTypeRepository => new GenericRepository<BikeType>(_dbContext);
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing) _dbContext.Dispose();
+                _disposed = true;
+            }
+        }
+    }
+}
